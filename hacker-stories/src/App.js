2a9507360ab2551f4a6a81d48241c81cc109ad42
insoo223 <insoo@hotmail.com>
2023-07-09 10:41:35 +0900

    string const in JSX

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,10 +1,17 @@
 
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+}
+
 function App() {
   const title = 'React';
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1> 
+        {welcome.greeting} {welcome.title}
+      </h1>
 
       <label htmlFor="search">Search</label>
       <input id="search" type="text" />
