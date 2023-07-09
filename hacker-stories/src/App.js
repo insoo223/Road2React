f2205d5f24509b133f7567b50e49747c5f601147
insoo223 <insoo@hotmail.com>
2023-07-09 11:20:50 +0900

    list or array

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,19 +1,36 @@
 
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.dev/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
-  const title = 'React';
-
+ 
   return (
     <div>
       <h1> 
-        Hello {getTitle('React')}
+        My Hacker Stories
       </h1>
 
       <label htmlFor="search">Search</label>
       <input id="search" type="text" />
+      
+      <h2> {list[0].title} {list[0].url} </h2>
+      <h2> {list[1].title} {list[1].url} </h2>
     </div>
   );
 }
