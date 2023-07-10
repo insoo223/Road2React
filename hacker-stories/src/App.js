b766ad12632a6d688c795e5786bad8772304de4f
insoo223 <insoo@hotmail.com>
2023-07-10 09:33:25 +0900

    state for input eventvalue

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,22 +1,4 @@
-
-const stories = [
-  {
-    title: 'React',
-    url: 'https://react.dev/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+import React from 'react';
 
 //function List()
 const List = (props) => {
@@ -42,8 +24,29 @@ const List = (props) => {
 //function App() 
 const App = () =>
 {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://react.dev/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleChange = event => {
-    console.log (event.target.value);
+    setSearchTerm (event.target.value);
   }
  
   return (
@@ -54,6 +57,11 @@ const App = () =>
 
       <label htmlFor="search">Search</label>
       <input id="search" type="text" onChange={handleChange}/>
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+
       <hr/>
       <List list={stories}/> 
     </div>
